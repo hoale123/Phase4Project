46ec95fdbed495909c3fdc409c8ca6683a00ee91
Hoa Le <Hoa_le9485@yahoo.com>
2022-01-24 16:34:25 -0500

    demo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Search from "./Search";
 
-
-function Header() {
-    return (
-        <header>
-            <h1>
-                Flat Reviews 
-            </h1>
-        </header>
-    )
+function Header({ onSearch }) {
+  return (
+    <header>
+      <h1>
+        <span className="logo" role="img">
+          ☮
+        </span>
+        gregslist
+      </h1>
+      <Search onSearch={onSearch} />
+    </header>
+  );
 }
-
 
 export default Header;
