79a0f2c0a76a63264c5442151496497e1c081d72
Jack DeFeo <jackd3243@gmail.com>
2022-01-24 14:49:58 -0500

    pushoing to main

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,8 +10,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
 
+   
+    <div className="App">
+        <Header></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ client/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+function Search() {
+    return (
+        <form className="searchbar" >
+          <input
+            type="text"
+            id="search"
+            placeholder="search free stuff"
+            />
+          <button type="submit">🔍</button>
+        </form>
+      );
+    }
